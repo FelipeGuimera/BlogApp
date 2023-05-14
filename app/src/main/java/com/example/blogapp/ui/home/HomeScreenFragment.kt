234f416f3960e3c79202ff264c8e387a52038803
Felipe <91702841+FelipeGuimera@users.noreply.github.com>
2023-05-14 16:33:18 -0300

    view binding

--- app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
+++ app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
@@ -4,9 +4,19 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.blogapp.R
+import com.example.blogapp.databinding.FragmentHomeScreenBinding
+import com.example.blogapp.ui.home.adapter.HomeScreenAdapter
 
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
+
+    private lateinit var binding: FragmentHomeScreenBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
+
+
     }
 }

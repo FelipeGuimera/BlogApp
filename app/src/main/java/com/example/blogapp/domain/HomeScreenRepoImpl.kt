2f14967bdacefb8567ffa3d34d3e02c4c0149ecd
Felipe <91702841+FelipeGuimera@users.noreply.github.com>
2023-05-16 13:46:53 -0300

    interface home screen

---
+++ app/src/main/java/com/example/blogapp/domain/HomeScreenRepoImpl.kt
@@ -0,0 +1,11 @@
+package com.example.blogapp.domain
+
+import com.example.blogapp.core.Resource
+import com.example.blogapp.data.model.Post
+
+class HomeScreenRepoImpl(private val dataSource: HomeScreenDataSource): HomeScreenRepo {
+    override suspend fun getLatestPosts(): Resource<List<Post>> {
+        TODO("Not yet implemented")
+    }
+
+}

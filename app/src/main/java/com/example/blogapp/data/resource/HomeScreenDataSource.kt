ec8fd84d62eeb031e2d8255d2a83c38aa7203d61
Felipe <91702841+FelipeGuimera@users.noreply.github.com>
2023-05-17 11:44:49 -0300

    datasource

---
+++ app/src/main/java/com/example/blogapp/data/resource/HomeScreenDataSource.kt
@@ -0,0 +1,21 @@
+package com.example.blogapp.data.resource
+
+import com.example.blogapp.core.Resource
+import com.example.blogapp.data.model.Post
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.coroutines.tasks.await
+
+class HomeScreenDataSource {
+
+    suspend fun getLatestPosts(): Resource<List<Post>> {
+        val postList = mutableListOf<Post>()
+        val querySnapshot = FirebaseFirestore.getInstance().collection("posts").get().await()
+        for (post in querySnapshot.documents) {
+            post.toObject(Post::class.java)?.let {fbPost->
+                postList.add(fbPost)
+            }
+        }
+        return Resource.Success(postList)
+    }
+}

47e8ccbef657231c811e1e49d94b3ee3bab1e3df
Felipe <91702841+FelipeGuimera@users.noreply.github.com>
2023-05-24 12:39:38 -0300

    .

--- app/src/main/java/com/example/blogapp/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/blogapp/ui/camera/CameraFragment.kt
@@ -1,8 +1,18 @@
 package com.example.blogapp.ui.camera
 
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.blogapp.R
 
 
 class CameraFragment : Fragment(R.layout.fragment_camera) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    }
+
 }

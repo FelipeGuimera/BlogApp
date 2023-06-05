c27e52a2a302b4f9e2d5aef46bdb16873044d17d
Felipe <91702841+FelipeGuimera@users.noreply.github.com>
2023-06-04 21:26:11 -0300

    time ago obj

--- app/src/main/java/com/example/blogapp/data/remote/home/HomeScreenDataSource.kt
+++ app/src/main/java/com/example/blogapp/data/remote/home/HomeScreenDataSource.kt
@@ -2,6 +2,7 @@ package com.example.blogapp.data.remote.home
 
 import com.example.blogapp.core.Result
 import com.example.blogapp.data.model.Post
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
@@ -11,7 +12,8 @@ class HomeScreenDataSource {
         val postList = mutableListOf<Post>()
         val querySnapshot = FirebaseFirestore.getInstance().collection("posts").get().await()
         for (post in querySnapshot.documents) {
-            post.toObject(Post::class.java)?.let {fbPost->
+            post.toObject(Post::class.java)?.let { fbPost ->
+                fbPost.apply { created_at = post.getTimestamp("created_at", DocumentSnapshot.ServerTimestampBehavior.ESTIMATE)?.toDate() }
                 postList.add(fbPost)
             }
         }

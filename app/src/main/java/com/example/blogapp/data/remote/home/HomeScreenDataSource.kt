5ee1bbf760438cd4005ff8ab12e508352af7ee77
Felipe <91702841+FelipeGuimera@users.noreply.github.com>
2023-06-07 11:54:28 -0300

    flow

--- app/src/main/java/com/example/blogapp/data/remote/home/HomeScreenDataSource.kt
+++ app/src/main/java/com/example/blogapp/data/remote/home/HomeScreenDataSource.kt
@@ -5,31 +5,52 @@ import com.example.blogapp.data.model.Post
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.google.protobuf.Value
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class HomeScreenDataSource {
 
-    suspend fun getLatestPosts(): Result<List<Post>> {
+    @ExperimentalCoroutinesApi
+    suspend fun getLatestPosts(): kotlinx.coroutines.flow.Flow<Result<List<Post>>> = callbackFlow {
         val postList = mutableListOf<Post>()
 
+        var postReference: Query? = null
 
-        withContext(Dispatchers.IO) {
-            val querySnapshot = FirebaseFirestore.getInstance().collection("posts")
-                .orderBy("created_at", Query.Direction.DESCENDING).get().await()
-            for (post in querySnapshot.documents) {
-                post.toObject(Post::class.java)?.let { fbPost ->
-                    fbPost.apply {
-                        created_at = post.getTimestamp(
-                            "created_at",
-                            DocumentSnapshot.ServerTimestampBehavior.ESTIMATE
-                        )?.toDate()
-                    }
-                    postList.add(fbPost)
-                }
-            }
+        try {
+            postReference = FirebaseFirestore.getInstance().collection("posts")
+                .orderBy("created_at", Query.Direction.DESCENDING)
+        } catch (e: Throwable) {
+            close(e)
         }
-        return Result.Success(postList)
+
+        val suscription = postReference?.addSnapshotListener { value, error ->
+            if (value == null) return@addSnapshotListener
+
+            try {
+                postList.clear()
+                for (post in value.documents) {
+                    post.toObject(Post::class.java)?.let { fbPost ->
+                        fbPost.apply {
+                            created_at = post.getTimestamp(
+                                "created_at",
+                                DocumentSnapshot.ServerTimestampBehavior.ESTIMATE
+                            )?.toDate()
+                        }
+                        postList.add(fbPost)
+                    }
+                }
+            } catch (e: Exception) {
+                close(e)
+            }
+
+            trySend(Result.Success(postList)).isSuccess
+        }
+        awaitClose { suscription?.remove() }
     }
 }
+
--- app/src/main/java/com/example/blogapp/presentation/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/blogapp/presentation/home/HomeScreenViewModel.kt
@@ -6,21 +6,29 @@ import androidx.lifecycle.liveData
 import com.example.blogapp.core.Result
 import com.example.blogapp.domain.home.HomeScreenRepo
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import java.lang.Exception
 
-class HomeScreenViewModel(private val repo: HomeScreenRepo): ViewModel() {
+class HomeScreenViewModel(private val repo: HomeScreenRepo) : ViewModel() {
 
     fun fetchLatestPosts() = liveData(Dispatchers.IO) {
         emit(Result.Loading())
-        try {
-            emit(repo.getLatestPosts())
-        }catch (e: Exception){
-            emit(Result.Failure(e))
+        kotlin.runCatching {
+            repo.getLatestPosts()
+        }.onSuccess {   flowlist->
+            flowlist.collect{
+                emit(it)
+            }
+        }.onFailure {
+            emit(Result.Failure(Exception(it.message)))
         }
+
+
     }
 }
 
 
-class HomeScreenViewModelFactory(private val repo: HomeScreenRepo): ViewModelProvider.Factory {
+class HomeScreenViewModelFactory(private val repo: HomeScreenRepo) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(HomeScreenRepo::class.java).newInstance(repo)
     }

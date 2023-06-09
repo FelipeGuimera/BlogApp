95423be8c2dc353d4492bd1b8a521536af1490af
Felipe <91702841+FelipeGuimera@users.noreply.github.com>
2023-06-09 12:08:33 -0300

    likes

--- app/src/main/java/com/example/blogapp/ui/home/adapter/HomeScreenAdapter.kt
+++ app/src/main/java/com/example/blogapp/ui/home/adapter/HomeScreenAdapter.kt
@@ -4,15 +4,26 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.blogapp.R
 import com.example.blogapp.core.BaseViewHolder
 import com.example.blogapp.core.TimeUtils
 import com.example.blogapp.data.model.Post
 import com.example.blogapp.databinding.PostItemViewBinding
 
-class HomeScreenAdapter(private val postList: List<Post>) :
+class HomeScreenAdapter(
+    private val postList: List<Post>,
+    private val onPostClickListener: OnPostClickListener
+) :
     RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    private var postClickListener: OnPostClickListener? = null
+
+    init {
+        postClickListener = onPostClickListener
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
         val itemBinding =
@@ -29,7 +40,7 @@ class HomeScreenAdapter(private val postList: List<Post>) :
     override fun getItemCount(): Int = postList.size
 
 
-    private class HomeScreenViewHolder(
+    class HomeScreenViewHolder(
         val binding: PostItemViewBinding,
         val context: Context
     ) : BaseViewHolder<Post>(binding.root) {
@@ -38,11 +49,15 @@ class HomeScreenAdapter(private val postList: List<Post>) :
             addPostTimeStamp(item)
             setupPostImage(item)
             setupPostDescription(item)
+            tintHeartIcon(item)
+            setupLikeCount(item)
+            setLikeClickAction(item)
         }
 
         private fun setupProfileInfo(post: Post) {
-            Glide.with(context).load(post.profile_picture).centerCrop().into(binding.profilePicture)
-            binding.profileName.text = post.profile_name
+            Glide.with(context).load(post.poster?.profile_picture).centerCrop()
+                .into(binding.profilePicture)
+            binding.profileName.text = post.poster?.username
         }
 
         private fun addPostTimeStamp(post: Post) {
@@ -64,5 +79,46 @@ class HomeScreenAdapter(private val postList: List<Post>) :
             }
         }
 
+        private fun tintHeartIcon(post: Post) {
+            if (!post.liked) {
+                binding.likeBtn.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        context,
+                        R.drawable.ic_baseline_favorite_border_24
+                    )
+                )
+                binding.likeBtn.setColorFilter(ContextCompat.getColor(context, R.color.black))
+            } else {
+                binding.likeBtn.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        context,
+                        R.drawable.ic_baseline_favorite_24
+                    )
+                )
+                binding.likeBtn.setColorFilter(ContextCompat.getColor(context, R.color.red_like))
+            }
+        }
+
+        private fun setupLikeCount(post: Post) {
+            if (post.likes > 0) {
+                binding.likeCount.visibility = View.VISIBLE
+                binding.likeCount.text = "${post.likes} likes"
+            } else {
+                binding.likeCount.visibility = View.GONE
+            }
+        }
+
+        private fun setLikeClickAction(post: Post) {
+            binding.likeBtn.setOnClickListener {
+                if (post.liked) post.apply { liked = false } else post.apply { liked = true }
+                tintHeartIcon(post)
+                postClickListener?.onLikeButtonClick(post, post.liked)
+            }
+
+        }
     }
+}
+
+interface OnPostClickListener {
+    fun onLikeButtonClick(post: Post, liked: Boolean)
 }
